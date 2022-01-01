e4128228923bdbfc627e8ceb35c4e1b40cb22232
vishwampandya <vishwampandya00@gmail.com>
2022-01-01 19:21:29 +0530

    setting up things

---
+++ src/main/java/com/sampleproject/basicuniversity/BasicuniversityApplication.java
@@ -0,0 +1,13 @@
+package com.sampleproject.basicuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicuniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicuniversityApplication.class, args);
+	}
+
+}
